c73039de4580a2632f74fa82423288d37d485b78
Eric <ericyoung111@gmail.com>
2022-02-13 07:19:13 -0700

    contact form

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -3,6 +3,27 @@ import React from 'react';
 function Resume (props) {
     return (
         <section>
+            <h2>Proficiencies</h2>
+            <ul class="proficiency-list">
+                <li class="proficiency-item">
+                    Javascript
+                </li>
+                <li class="proficiency-item">
+                    React
+                </li>
+                <li class="proficiency-item">
+                    React
+                </li>
+                <li class="proficiency-item">
+                    Express
+                </li>
+                <li class="proficiency-item">
+                    Mysql
+                </li>
+                <li class="proficiency-item">
+                    MongoDB
+                </li>
+            </ul>
             {/* <a href="/assets/resume.pdf" width="800px" height="2100px" /> */}
             <embed
                 src="/assets/eric-young-resume-web-development.pdf" 
